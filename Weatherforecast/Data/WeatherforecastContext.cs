7dd6798d8e879dc6252491de973cb8fa8557a06d
Dasari <bharathi.dasari@capgemini.com>
2021-11-30 19:00:02 +0530

    TrailCommit

---
+++ Weatherforecast/Data/WeatherforecastContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Weatherforecast;
+
+namespace Weatherforecast.Data
+{
+    public class WeatherforecastContext : DbContext
+    {
+        public WeatherforecastContext (DbContextOptions<WeatherforecastContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Weatherforecast.Student> Student { get; set; }
+    }
+}
---
+++ Weatherforecast/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weatherforecast
+{
+    [Table(name: "Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Location { get; set; }
+    }
+}
